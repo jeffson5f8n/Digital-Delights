c2de511bc838471cc2afbb289fabeae535053a29
jefferson <159613142+jeffson5f8n@users.noreply.github.com>
2025-05-06 19:01:17 +0300

    Update CartContext.jsx

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -1,51 +1,30 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from 'react';
 
-// Create a CartContext
 const CartContext = createContext();
 
-// CartProvider component
+export const useCart = () => {
+  return useContext(CartContext);
+};
+
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  // Function to add product to cart
   const addToCart = (product) => {
-    setCart((prevCart) => {
-      const existingProduct = prevCart.find((item) => item.id === product.id);
-
-      if (existingProduct) {
-        // If product exists, increment quantity
-        return prevCart.map((item) =>
-          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      } else {
-        // If product does not exist, add new product with quantity 1
-        return [...prevCart, { ...product, quantity: 1 }];
-      }
-    });
+    const cartItem = { ...product, cartItemId: Date.now() }; // Unique ID for each item
+    setCart((prevCart) => [...prevCart, cartItem]);
   };
 
-  // Function to remove product from cart
-  const removeItem = (id) => {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  const removeFromCart = (cartItemId) => {
+    setCart((prevCart) => prevCart.filter(item => item.cartItemId !== cartItemId));
   };
 
-  // Function to update quantity of product in the cart
-  const updateQuantity = (id, quantity) => {
-    setCart((prevCart) =>
-      prevCart.map((item) =>
-        item.id === id ? { ...item, quantity: quantity } : item
-      )
-    );
+  const calculateTotal = () => {
+    return cart.reduce((acc, product) => acc + parseInt(product.product_cost), 0);
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeItem, updateQuantity }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, calculateTotal }}>
       {children}
     </CartContext.Provider>
   );
-};
-
-// Custom hook to access cart context
-export const useCart = () => {
-  return useContext(CartContext);
 };
